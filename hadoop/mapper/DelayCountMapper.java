367260828dd3f9e9f23960d1063e6fa9f5a85c9c
Juan Bang <juanbang0510@gmail.com>
2022-07-21 17:07:26 +0900

    added hadoop mapper classes

---
+++ hadoop/mapper/DelayCountMapper.java
@@ -0,0 +1,42 @@
+package com.dodam.mapper;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import com.dodam.common.AirlinePerformanceParser;
+
+public class DelayCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	private String workType;
+	private final static IntWritable outputValue = new IntWritable(1);
+	private Text outputKey = new Text();
+
+	// 최초 맵 생성될대 한번 실행되는(초기화 되는 메서드)
+	@Override
+	protected void setup(Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		workType = context.getConfiguration().get("workType");
+	}
+
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		AirlinePerformanceParser parser = new AirlinePerformanceParser(value);
+		outputKey.set(parser.getYear() + "," + parser.getMonth());
+		if (workType.equals("departure")) {
+			if(parser.getDepartureDelayTime()> 0) {
+				context.write(outputKey, outputValue);
+			}
+		} else if (workType.equals("arrival")) {
+			if(parser.getArrivalDelayTime()> 0) {
+				context.write(outputKey, outputValue);
+			}
+		}
+	}
+
+}
---
+++ hadoop/mapper/DelayCountMapperWithCounter.java
@@ -0,0 +1,62 @@
+package com.dodam.mapper;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import com.dodam.common.AirlinePerformanceParser;
+import com.dodam.common.DelayCounters;
+
+public class DelayCountMapperWithCounter extends Mapper<LongWritable, Text, Text, IntWritable> {
+	private String workType;
+	private final static IntWritable outputValue = new IntWritable(1);
+	private Text outputKey = new Text();
+
+	// 최초 맵 생성될대 한번 실행되는(초기화 되는 메서드)
+	@Override
+	protected void setup(Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		workType = context.getConfiguration().get("workType");
+	}
+
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		AirlinePerformanceParser parser = new AirlinePerformanceParser(value);
+		outputKey.set(parser.getYear() + "," + parser.getMonth());
+		// -D workType=[D,A]-[JFK]
+		// -D workType=D-999 --> 다 뽑아라
+		if (workType.equals("departure")) {
+			if (parser.isDepartureDelayAvailable()) {
+				if (parser.getDepartureDelayTime() > 0) {
+					context.write(outputKey, outputValue);
+				} else if(parser.getDepartureDelayTime() == 0) {	
+					context.getCounter(DelayCounters.scheduled_departure).increment(1);
+				} else if(parser.getDepartureDelayTime() < 0) {	
+					context.getCounter(DelayCounters.early_departure).increment(1);
+				}
+			} else {
+				context.getCounter(DelayCounters.not_available_departure).increment(1);
+			}
+		} else if (workType.equals("arrival")) {
+			if (parser.isArrivalDelayAvailable()) {
+				if (parser.getArrivalDelayTime() > 0) {
+					context.write(outputKey, outputValue);
+				} else if(parser.getArrivalDelayTime() == 0) {	
+					context.getCounter(DelayCounters.scheduled_arrival).increment(1);
+				} else if(parser.getArrivalDelayTime() < 0) {	
+					context.getCounter(DelayCounters.early_arrival).increment(1);
+				}
+			} else {
+				context.getCounter(DelayCounters.not_available_arrival).increment(1);
+			}
+		}
+
+	}
+
+}
